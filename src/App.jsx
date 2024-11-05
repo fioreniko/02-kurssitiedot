9c78bcf19660d54b8c5d903911d9958c48bf0925
Fiorella Dalla Riva <s2400314@edu.bc.fi>
2024-11-05 22:10:20 +0200

    1.7 - unicafe step 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,50 @@
 /*
 https://fullstackopen.com/en/part1/a_more_complex_state_debugging_react_apps#exercises-1-6-1-14
-tehtävät 1.6
+tehtävät 1.7
 */
 
 import { useState } from "react";
 
 const App = () => {
-  // save clicks of each button to its own state
+  const goodWeight = 1;
+  const neutralWeight = 0;
+  const badWeight = -1;
+
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const all = good + neutral + bad;
+
+  const handleGoodClick = () => {
+    setGood(good + 1);
+  };
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1);
+  };
+
+  const handleBadClick = () => {
+    setBad(bad + 1);
+  };
+
+  const average =
+    all === 0 ? 0 : (good * goodWeight + neutral * neutralWeight + bad * badWeight) / all;
+  const positive = all === 0 ? 0 : (100 * good) / all;
 
   return (
     <div>
       <h2>give feedback</h2>
 
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <button onClick={handleGoodClick}>good</button>
+      <button onClick={handleNeutralClick}>neutral</button>
+      <button onClick={handleBadClick}>bad</button>
       <h2>statistics</h2>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive}%</p>
     </div>
   );
 };
