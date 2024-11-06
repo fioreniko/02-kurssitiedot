add8e0c7a1b1a76f2efa5ac97cf21e8bcccb1c85
Fiorella Dalla Riva <s2400314@edu.bc.fi>
2024-11-06 22:01:38 +0200

    1.11 - unicafe step 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 /*
 https://fullstackopen.com/en/part1/a_more_complex_state_debugging_react_apps#exercises-1-6-1-14
-tehtävät 1.10
+tehtävät 1.11
 */
 
 import { useState } from "react";
--- src/component/Statistics.jsx
+++ src/component/Statistics.jsx
@@ -16,12 +16,16 @@ const Statistics = ({ good, neutral, bad }) => {
       {all > 0 ? (
         <>
           <h2>statistics</h2>
-          <StatisticLine text="good" value={good} />
-          <StatisticLine text="neutral" value={neutral} />
-          <StatisticLine text="bad" value={bad} />
-          <StatisticLine text="all" value={all} />
-          <StatisticLine text="average" value={average} />
-          <StatisticLine text="positive" value={positive + "%"} />
+          <table>
+            <tbody>
+              <StatisticLine text="good" value={good} />
+              <StatisticLine text="neutral" value={neutral} />
+              <StatisticLine text="bad" value={bad} />
+              <StatisticLine text="all" value={all} />
+              <StatisticLine text="average" value={average} />
+              <StatisticLine text="positive" value={positive + "%"} />
+            </tbody>
+          </table>
         </>
       ) : (
         <p>No feedback given</p>
